50fd89058a04b0baa96805b91af9ecfeec64d42a
codergon <alphagon4@gmail.com>
2021-12-14 21:32:38 +0100

    Wrote codebase all in JS🎈

--- frontend/src/statics/BottomNavigationBar.js
+++ frontend/src/statics/BottomNavigationBar.js
@@ -23,69 +23,6 @@ const BottomNavigationBar = ({ NavLink, darkTheme }) => {
   return (
     <footer className="ft_sm">
       <ul className="ft_sm_inn">
-        <li className="ft_sm_li">
-          <NavLink
-            style={({ isActive }) => {
-              return {
-                display: "flex",
-                fontSize: isActive ? "14px" : "13px",
-                opacity: isActive ? "0.9" : "0.6",
-                alignItems: "center",
-                flexDirection: "column",
-              };
-            }}
-            to={`/`}
-            key={"home"}
-          >
-            <i className="uil uil-estate"></i>
-          </NavLink>
-        </li>
-        <li className="ft_sm_li">
-          <NavLink
-            style={({ isActive }) => {
-              return {
-                display: "flex",
-                fontSize: isActive ? "14px" : "13px",
-                opacity: isActive ? "0.9" : "0.6",
-                alignItems: "center",
-                flexDirection: "column",
-              };
-            }}
-            to={`/elections`}
-            key={"elections"}
-          >
-            <i className="uil uil-mailbox"></i>
-          </NavLink>
-        </li>
-
-        {/* Transfer Section */}
-        <li className="ft_sm_li">
-          <NavLink
-            style={({ isActive }) => {
-              return {
-                width: "35px",
-                height: "35px",
-                display: "flex",
-                borderRadius: "100%",
-                alignItems: "center",
-                justifyContent: "center",
-                background: "var(--main-col)",
-                color: !darkTheme ? "#fff" : "",
-                border: isActive
-                  ? darkTheme
-                    ? "1px solid var(--wht)"
-                    : "1px solid #666"
-                  : "none",
-                fontSize: isActive ? "14px" : "13px",
-              };
-            }}
-            to={`/transfer`}
-            key={"transfer"}
-          >
-            <i className="uil uil-exchange"></i>
-          </NavLink>
-        </li>
-
         <li className="ft_sm_li" onClick={setMode}>
           <div
             style={{
